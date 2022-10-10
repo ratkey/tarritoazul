7fba0f821a9d202c010e2abeae34f0e7e4dad207
Abril <Tic-290012@utnay.edu.mx>
2022-10-10 10:46:26 -0600

    agregue registro.asp y añadí nombre y emilio

---
+++ tarritoazul.com/tarritoazul.com/forms/registro.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="registro.aspx.cs" Inherits="tarritoazul.com.forms.registro" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+
+            <tr>
+                <td>
+                    <asp:Label ID="LbNombre" runat="server" Text="Nombre"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="TbNombre" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+             <tr>
+                <td>
+                    <asp:Label ID="LbEmail" runat="server" Text="Email"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="TbEmail" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+
+    </form>
+</body>
+</html>
---
+++ tarritoazul.com/tarritoazul.com/forms/registro.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tarritoazul.com.forms
+{
+
+
+    public partial class registro
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control LbNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LbNombre;
+
+        /// <summary>
+        /// Control TbNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbNombre;
+
+        /// <summary>
+        /// Control LbEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LbEmail;
+
+        /// <summary>
+        /// Control TbEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbEmail;
+    }
+}

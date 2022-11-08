28e1fd4d0fa38a6bf7d5bea450ea0af8884377d3
CotherArt <64714914+CotherArt@users.noreply.github.com>
2022-11-07 18:37:06 -0700

    si

--- tarritoazul.com/forms/addProducto.aspx.cs
+++ tarritoazul.com/forms/addProducto.aspx.cs
@@ -23,28 +23,12 @@ namespace tarritoazul.com.forms
             insertProducto();
         }
 
-        protected string generateProductCode(string nombre)
-        {
-            Random rnd = new Random();
-            nombre = nombre.ToUpper();
-            if (nombre.Length > 5)
-            {
-                nombre = nombre.Substring(0, 5);
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                int let = rnd.Next(65, 90);
-                nombre += (char)let;
-            }
-            return nombre;
-        }
-
         protected void FileUpload_SaveBtn_Click(object sender, EventArgs e)
         {
             subirArchivos();
         }
 
-        protected void subirArchivos(int id_producto)
+        protected void subirArchivos()
         {
             //ruta para cuardar los archivos
             string filepath = "D:\\Projects\\web\\tarritoazul\\tarritoazul.com\\imgs\\producto\\";
@@ -67,9 +51,6 @@ namespace tarritoazul.com.forms
                         //guardar actual archivo en el directorio
                         archivo.SaveAs(ruta_guardado);
                         cantidad_archivos++;
-
-                        //guardarlo en la BD
-                        insertMedia(fn, "imagen", id_producto);
                     }
                     catch (Exception ex)
                     {
@@ -85,18 +66,6 @@ namespace tarritoazul.com.forms
             {
                 FileUploadStatus.Text = "Please select a file to upload.";
             }
-        }
-
-        protected int getIdProducto(string product_code)
-        {
-            con.Open();
-
-            string SQLSelect = String.Format("select id_producto from PRODUCTOS" +
-            "where codigo_producto == '{0}';", product_code);
-
-            SqlCommand cmd = new SqlCommand(SQLSelect, con);
-            cmd.ExecuteNonQuery().;
-            con.Close();
         }
 
         protected void insertMedia(string src_url, string tipo, int id_producto)
@@ -138,6 +107,22 @@ namespace tarritoazul.com.forms
 
             string script = "alert('Producto registrado correctamente 👍');";
             ScriptManager.RegisterStartupScript(this, typeof(Page), "alerta", script, true);
+        }
+
+        protected string generateProductCode(string nombre)
+        {
+            Random rnd = new Random();
+            nombre = nombre.ToUpper();
+            if (nombre.Length > 5)
+            {
+                nombre = nombre.Substring(0, 5);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                int let = rnd.Next(65, 90);
+                nombre += (char)let;
+            }
+            return nombre;
         }
     }
 }

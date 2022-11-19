02ce653c55b6f35308fe923b1de1df0e95e36b57
CotherArt <64714914+CotherArt@users.noreply.github.com>
2022-11-18 18:06:56 -0700

    boton Administrar agregado

--- tarritoazul.com/index.aspx.designer.cs
+++ tarritoazul.com/index.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace tarritoazul.com
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// btnAdmin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAdmin;
+
+        /// <summary>
         /// btnRegistrarse control.
         /// </summary>
         /// <remarks>

2cba2a6f91eee48286e1f3f22fe77070f5d45b75
Francisco <tic-290037@utnay.edu.mx>
2022-11-23 12:00:10 -0700

    lo termine

---
+++ tarritoazul.com/forms/DetalleProducto.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="DetalleProducto.aspx.cs" Inherits="tarritoazul.com.forms.DetalleProducto" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Contenido" runat="server">
+    <table style="text-align:center" aling="center" border="1">
+        <tr>
+            <td style="width: 350px;">
+                <%--Td de relleno para centrar la tabla pq no se--%>
+            </td>
+            <%--td de la imagen--%>
+            <td style="max-width: 150px; overflow: hidden;">
+                <iframe src="https://m.media-amazon.com/images/I/71fCb8S9dtL._SY500_.jpg" height="600" width="510"> </iframe>
+            </td>
+
+            <%--td de los labels y botones--%>
+            <td>
+                <asp:Label ID="LbProducto" runat="server" Text="AretesCoso"></asp:Label>
+                <br /> <br />
+                <asp:Label ID="LbPrecio" runat="server" Text="15$"></asp:Label>
+                <br /> <br />
+                <asp:Label ID="LbDescripcion" runat="server" Text="Aretes con forma de coso que huelen a lima"></asp:Label>
+                <br /> <br />
+                <asp:Label ID="LbCantidad" runat="server" Text="Cantidad:"></asp:Label>
+                <asp:TextBox ID="TbCantidad" runat="server" TextMode="Number" Text="1" width="30px"></asp:TextBox>
+                 <asp:RangeValidator id="RangeCantidad"
+          	 ControlToValidate="TbCantidad"
+          	 MinimumValue="0"
+          	 MaximumValue="999"
+          	 Type="Integer"
+          	 EnableClientScript="false"
+          	 Text="*"
+          	 runat="server"/>
+                <br /><br />
+                 <asp:Button ID="BtnAgregar" runat="server" Text="Agregar al carrito" CssClass="btn btn-solid-green" />
+                <br /><br />
+                 <asp:Button ID="BtnComprar" runat="server" Text="Comprar Ahora" CssClass="btn btn-solid-green" />
+            </td>
+        </tr>
+         <asp:ValidationSummary 
+                              id="Summary" 
+                              runat="server"/>
+    </table>
+</asp:Content>
---
+++ tarritoazul.com/forms/DetalleProducto.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tarritoazul.com.forms
+{
+
+
+    public partial class DetalleProducto
+    {
+
+        /// <summary>
+        /// Control LbProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LbProducto;
+
+        /// <summary>
+        /// Control LbPrecio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LbPrecio;
+
+        /// <summary>
+        /// Control LbDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LbDescripcion;
+
+        /// <summary>
+        /// Control LbCantidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LbCantidad;
+
+        /// <summary>
+        /// Control TbCantidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbCantidad;
+
+        /// <summary>
+        /// Control RangeCantidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RangeCantidad;
+
+        /// <summary>
+        /// Control BtnAgregar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnAgregar;
+
+        /// <summary>
+        /// Control BtnComprar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnComprar;
+
+        /// <summary>
+        /// Control Summary.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary Summary;
+    }
+}

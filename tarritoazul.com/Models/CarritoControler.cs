4080200551b81c366380a950e8289f04ef1cb4c8
CotherUT <tic-290025@utnay.edu.mx>
2022-11-28 11:29:17 -0700

    se aggrego CarritoControler.cs

---
+++ tarritoazul.com/Models/CarritoControler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tarritoazul.com.Models
+{
+    public class CarritoControler
+    {
+        //Agregar un producto al carrito de la Session
+        public static void AddProducto(Producto p)
+        {
+            if (HttpContext.Current.Session["carrito"] == null) return;
+
+            List<Producto> lista = (List<Producto>)HttpContext.Current.Session["carrito"];
+            lista.Add(p);
+            HttpContext.Current.Session["carrito"] = lista;
+        }
+    }
+}

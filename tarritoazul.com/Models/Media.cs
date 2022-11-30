11278a49af02f2bb4b5b1eca382b1aea4963fb38
CotherArt <64714914+CotherArt@users.noreply.github.com>
2022-11-29 18:59:29 -0700

    hotfix Media.cs

--- tarritoazul.com/Models/Media.cs
+++ tarritoazul.com/Models/Media.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Web;
-using System.Web.Services.Description;
-using System.Web.UI;
-using System.Windows.Forms;
-using tarritoazul.com.taTableAdapters;
-
-namespace tarritoazul.com.Models
+﻿namespace tarritoazul.com.Models
 {
     public class Media
     {
-
         public Media()
         {
             Id_Media = -1;
@@ -26,5 +11,10 @@ namespace tarritoazul.com.Models
         public string Src_Url { get; set; }
         public string Tipo { get; set; }
         public int Id_Producto { get; set; }
+
+        public override string ToString()
+        {
+            return "id_media: " +Id_Media+ "src_url: " +Src_Url+ "tipo: " +Tipo+ "id_producto: " + Id_Producto;
+        }
     }
 }
